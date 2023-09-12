86f6deedcac743f0d8e27a3261ac92a597fd62f9
Mohammad Shams Tabrez <shamstabrez.raw@gmail.com>
2023-09-12 12:41:33 -0500

    Initial commit

---
+++ src/main/java/com/shamstabrez16/kafkaconsumer/listener/KafkaConsumer.java
@@ -0,0 +1,21 @@
+package com.shamstabrez16.kafkaconsumer.listener;
+
+import com.shamstabrez16.kafkaconsumer.model.User;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaConsumer {
+
+    @KafkaListener(topics = "java_in_use_topic",groupId = "group_id")
+    public void consume(String message) {
+        System.out.println("Consumed message: " + message);
+    }
+
+
+    @KafkaListener(topics = "Kafka_Example_json", groupId = "group_json",
+            containerFactory = "userKafkaListenerFactory")
+    public void consumeJson(User user) {
+        System.out.println("Consumed JSON Message: " + user);
+    }
+}
